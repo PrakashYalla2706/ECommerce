0005a529d1057cddffe0bdf1e8378200143f7faa
PrakashYalla2706 <119692861+PrakashYalla2706@users.noreply.github.com>
2024-09-27 18:23:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import path from "path";
 import cors from "cors";
 import bodyParser from "body-parser";
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 const app = express();
 
 app.use(bodyParser.json());
