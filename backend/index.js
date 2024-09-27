4f0b07ac2ba17254be04204f7b912abd2ce035a6
PrakashYalla2706 <119692861+PrakashYalla2706@users.noreply.github.com>
2024-09-27 17:07:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import dotenv from "dotenv";  // Import dotenv
 // Load environment variables from .env file
 dotenv.config();
 
-const port = process.env.PORT || 4000;
+const port =  4000;
 const app = express();
 
 app.use(bodyParser.json());
