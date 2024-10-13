fd4bc2636139a38c895a6ce62760689f7b1468c5
PrakashYalla2706 <119692861+PrakashYalla2706@users.noreply.github.com>
2024-10-13 22:22:05 +0530

    Update NewCollections.jsx

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -10,7 +10,7 @@ export const NewCollections = () => {
     .then((data)=>setNew_collection(data));
   },[])
   return (
-    <div className='new-collections'>
+    <div id = 'new-collections' className='new-collections'>
         <h1>NEW COLLECTIONS</h1>
         <hr />
         <div className="collections">
